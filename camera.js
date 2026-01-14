a8236895e6a801741617261fd9abbd7f50a84260
Jonatas Andrade <jonatasandrade8@gmail.com>
2026-01-13 23:33:39 -0300

    Update camera.js

--- camera.js
+++ camera.js
@@ -36,10 +36,10 @@ const APP_DATA = {
             "Mateus": ["Loja 04"],
             "Amarildo": ["Loja 05"],
             "Cristiane": ["Loja 07"],
-            "Markson": ["Loja 08"]
+            "Markson": ["Loja 08","Loja 04"]
         },
         "Carrefour": {
-            "Mateus": ["Zona Sul"]
+            "Markson": ["Zona Sul"]
         },
         "Superfácil": {
             "Neto": ["Emaús"],
@@ -948,7 +948,7 @@ async function sharePhotos() {
     const dateText = new Date().toLocaleString('pt-BR', { dateStyle: 'short', timeStyle: 'medium' });
 
     // 2. Cria a legenda idêntica à marca d'água (Ordem de leitura: Topo -> Baixo na foto)
-    const legendaCompartilhada = `*Qdelícia Frutas - Relatório Fotográfico*\n\n${tipoFotoText}\n${promotorText}\n${redeText}\n${lojaText}\n${dateText}\n\n(Fotos Anexadas: ${photos.length})`;
+    const legendaCompartilhada = `*Relatório Fotográfico*\n${tipoFotoText}\n${promotorText}\n${redeText}\n${lojaText}\n${dateText}\n(Fotos Anexadas: ${photos.length})`;
 
     try {
         const files = photos.slice(0, MAX_PHOTOS).map((img, i) => {
